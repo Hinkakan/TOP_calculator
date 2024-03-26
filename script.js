27fa27366d2c730a80af76cde7fe4ab394de537c
Bjarki <49208402+Hinkakan@users.noreply.github.com>
2024-03-26 12:10:03 +0100

    finally done

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function equals() {
     operate(Number(firstNumber), Number(numberHolder), operator);
     result = firstNumber;
     firstNumber = undefined;
+    numberHolder = ""
 };
 
 function operate(num1, num2, o) {
@@ -37,10 +38,22 @@ function operate(num1, num2, o) {
         r = divide(num1, num2);
     };
 
-    firstNumber = r;
-    display.textContent = r;
+    firstNumber = roundToFixedIfNeeded(r, 5);
+    display.textContent = firstNumber;
 
 };
+
+function roundToFixedIfNeeded(number, decimals) {
+    numberString = number.toString();
+    const decimalIndex = numberString.indexOf('.');
+    const numLength = numberString.length;
+
+    if (numLength-decimalIndex-1 > decimals) {
+        return number.toFixed(decimals);
+    } else {
+        return number;
+    };
+}
 
 
 function addCharacter(char, what) {
@@ -71,18 +84,32 @@ function clearAll() {
     operator = undefined;
     result = undefined;
     display.textContent = "";
-}
+};
+
+function del() {
+    if (display.textContent == "+" || display.textContent == "-" || display.textContent == "*" || display.textContent == "/") {
+        ;
+    } else if (numberHolder == "") {
+        ;
+    } else {
+        numberHolder = numberHolder.slice(0,-1);
+        display.textContent = numberHolder;
+    }
+};
 
 //Build calculator
 const numbers_container = document.querySelector("#numbers_container");
 const bottom_container = document.querySelector("#bottom_container");
 const right_container = document.querySelector("#right_container");
+const results_container = document.querySelector("#results_container");
 let display = document.querySelector("#results_bar");
 
 // Add numpad
 for (let i = 1; i <= 9; i++) {
     let newbutton = document.createElement("button");
     newbutton.classList.add("button");
+    newbutton.classList.add("num_button");
+    newbutton.id = `b${i}`;
     newbutton.textContent = i
     newbutton.addEventListener("click", (e) => addCharacter(e.target.textContent, "number"));
     numbers_container.appendChild(newbutton);
@@ -91,18 +118,21 @@ for (let i = 1; i <= 9; i++) {
 // BUILD UI
 // Add bottom row
 let newbutton = document.createElement("button");
-newbutton.textContent = "Clr";
+newbutton.textContent = ".";
 newbutton.classList.add("button");
-newbutton.addEventListener("click", () => clearAll());
+newbutton.classList.add("other_button");
+newbutton.addEventListener("click", (e) => addCharacter(e.target.textContent, "number"));
 bottom_container.appendChild(newbutton);
 let newbutton1 = document.createElement("button");
 newbutton1.textContent = "0";
 newbutton1.classList.add("button");
+newbutton1.classList.add("other_button");
 newbutton1.addEventListener("click", (e) => addCharacter(e.target.textContent, "number"));
 bottom_container.appendChild(newbutton1);
 let newbutton2 = document.createElement("button");
 newbutton2.textContent = "=";
 newbutton2.classList.add("button");
+newbutton2.classList.add("other_button");
 newbutton2.addEventListener("click", () => equals());
 bottom_container.appendChild(newbutton2);
 
@@ -110,26 +140,70 @@ bottom_container.appendChild(newbutton2);
 let newbutton3 = document.createElement("button");
 newbutton3.textContent = "/";
 newbutton3.classList.add("button");
+newbutton3.classList.add("other_button");
 newbutton3.addEventListener("click", (e) => addCharacter(e.target.textContent, "operator"));
 right_container.appendChild(newbutton3);
 let newbutton4 = document.createElement("button");
 newbutton4.textContent = "*";
 newbutton4.classList.add("button");
+newbutton4.classList.add("other_button");
 newbutton4.addEventListener("click", (e) => addCharacter(e.target.textContent, "operator"));
 right_container.appendChild(newbutton4);
 let newbutton5 = document.createElement("button");
 newbutton5.textContent = "-";
 newbutton5.classList.add("button");
+newbutton5.classList.add("other_button");
 newbutton5.addEventListener("click", (e) => addCharacter(e.target.textContent, "operator"));
 right_container.appendChild(newbutton5);
 let newbutton6 = document.createElement("button");
 newbutton6.textContent = "+";
 newbutton6.classList.add("button");
+newbutton6.classList.add("other_button");
 newbutton6.addEventListener("click", (e) => addCharacter(e.target.textContent, "operator"));
 right_container.appendChild(newbutton6);
+
+// Add more buttons
+let newbutton7 = document.createElement("button");
+newbutton7.textContent = "del";
+newbutton7.classList.add("button");
+newbutton7.classList.add("other_button");
+newbutton7.addEventListener("click", (e) => del());
+results_container.appendChild(newbutton7);
+let newbutton8 = document.createElement("button");
+newbutton8.textContent = "Clr";
+newbutton8.classList.add("button");
+newbutton8.classList.add("other_button");
+newbutton8.addEventListener("click", (e) => clearAll());
+results_container.appendChild(newbutton8);
 
 // Initiate empty variables
 let numberHolder = "";
 let firstNumber;
 let operator;
 let result;
+
+//add general eventlistner looking for keydown events. Get thje keydown-event, match it with a button, and do click() event
+document.addEventListener("keydown", (e) => {
+    const keyCode = e.code.replace("Numpad", "");
+    validValues = ["1","2","3","4","5","6","7","8","9"];
+    if (keyCode == "Enter") {
+        newbutton2.click();
+    } else if (keyCode == "Divide") {
+        newbutton3.click();
+    } else if (keyCode == "Multiply") {
+        newbutton4.click();
+    } else if (keyCode == "Subtract") {
+        newbutton5.click();
+    } else if (keyCode == "Add") {
+        newbutton6.click();
+    } else if (keyCode == "0") {
+        newbutton1.click();
+    } else if (keyCode == "Decimal") {
+        newbutton.click();
+    } else if (keyCode == "Delete") {
+        newbutton7.click();
+    } else {
+        let butt = document.querySelector(`#b${keyCode}`);
+        butt.click();
+    }
+})
